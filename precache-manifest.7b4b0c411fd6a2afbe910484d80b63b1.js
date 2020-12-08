1d89b732f2c9447502110d5745fb8541328e4d59
Luca Perticaroli <l.perticaroli@campus.uniurb.it>
2020-12-08 17:53:15 +0100

    Updates

---
+++ precache-manifest.7b4b0c411fd6a2afbe910484d80b63b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "672e7b52689525b2f99f5a7c5e52cd59",
+    "url": "/LemBeachSite/index.html"
+  },
+  {
+    "revision": "c06cf7cd804a4b6fd687",
+    "url": "/LemBeachSite/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "f11dddf2fd558c6a41ab",
+    "url": "/LemBeachSite/static/js/2.088e03eb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/LemBeachSite/static/js/2.088e03eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c06cf7cd804a4b6fd687",
+    "url": "/LemBeachSite/static/js/main.151251a5.chunk.js"
+  },
+  {
+    "revision": "8f65e65ddb26424c3902",
+    "url": "/LemBeachSite/static/js/runtime-main.d17938d2.js"
+  }
+]);
