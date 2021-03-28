e524ed1990f9f240cd530f85b6452beb1d70a2f2
lucapert <l.perticaroli@gmail.com>
2021-03-28 13:50:30 +0200

    added background image in carousel with dark overlay

--- lembeachsite/pages/index.tsx
+++ lembeachsite/pages/index.tsx
@@ -2,6 +2,11 @@ import styles from '../styles/Home.module.css';
 import CarouselReactB  from '../components/Carousel/Carousel'
 export default function Home() {
   return (
-    <CarouselReactB />
+    <>
+      <CarouselReactB />
+      <div style={ { height: '100px' } }> 
+
+      </div>
+    </>
   )
 }
