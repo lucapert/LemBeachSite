ee8fbcf959b7144d1ca0185d9b65f254761f13a9
lucapert <l.perticaroli@gmail.com>
2021-03-28 03:15:23 +0200

    changed carousel to crossfade

--- lembeachsite/pages/index.tsx
+++ lembeachsite/pages/index.tsx
@@ -3,42 +3,42 @@ import { Container, Row, Col, Navbar, Nav, NavDropdown, Form, FormControl, Butto
 import 'bootstrap/dist/css/bootstrap.min.css';
 export default function Home() {
   return (
-    <Carousel>
-    <Carousel.Item>
-      <img
-        className="d-block w-100"
-        src="/vendors/images/maldive.jpg"
-        alt="First slide"
-      />
-      <Carousel.Caption>
-        <h3>First slide label</h3>
-        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-      </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item>
-      <img
-        className="d-block w-100"
-        src="/vendors/images/mare1.jpg"
-        alt="Second slide"
-      />
-  
-      <Carousel.Caption>
-        <h3>Second slide label</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item>
-      <img
-        className="d-block w-100"
-        src="/vendors/images/mare.jpg"
-        alt="Third slide"
-      />
-  
-      <Carousel.Caption>
-        <h3>Third slide label</h3>
-        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-      </Carousel.Caption>
-    </Carousel.Item>
+    <Carousel fade>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="/vendors/images/maldive.jpg"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="/vendors/images/mare1.jpg"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="/vendors/images/mare.jpg"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
   </Carousel>
   )
 }
