2e81e402fe22a95f2ae37975afe9f40540e2eddf
lucapert <l.perticaroli@gmail.com>
2021-03-30 01:19:07 +0200

    fix card font

--- lembeachsite/components/Cards/Cards.tsx
+++ lembeachsite/components/Cards/Cards.tsx
@@ -7,7 +7,7 @@ const Cards = () => {
         <div className={ Styles.cardsContainer }>
             <Card Title="Corsi windsurf di gruppo" Color="red" LinkUrl="https://www.google.it" ImgUrl="https://fakeimg.pl/400x400/"/>    
             <Card Title="Corsi windsurf base" Color="red" LinkUrl="https://www.google.it" ImgUrl="https://fakeimg.pl/400x400/"/>    
-            <Card Title="Corsi windsurf medi" Color="red" LinkUrl="https://www.google.it" ImgUrl="https://fakeimg.pl/400x400/"/>    
+            <Card Title="Corsi windsurf intermedi" Color="red" LinkUrl="https://www.google.it" ImgUrl="https://fakeimg.pl/400x400/"/>    
             <Card Title="Corsi windsurf avanzati" Color="red" LinkUrl="https://www.google.it" ImgUrl="https://fakeimg.pl/400x400/"/>    
             <Card Title="Tesseramenti" Color="red" LinkUrl="https://www.google.it" ImgUrl="https://fakeimg.pl/400x400/"/>    
         </div>
