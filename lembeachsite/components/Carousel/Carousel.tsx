dec663b37b348743941f22238df928375410cc4b
lucapert <l.perticaroli@gmail.com>
2021-03-29 02:00:37 +0200

    added fadein text on carousel

--- lembeachsite/components/Carousel/Carousel.tsx
+++ lembeachsite/components/Carousel/Carousel.tsx
@@ -4,52 +4,43 @@ import { Container, Row, Col, Navbar, Nav, NavDropdown, Form, FormControl, Butto
 
 const CarouselReactB = () => {
     return (
+        <div className={ Styles.carouselContainer }>
             <Carousel fade>
                 <Carousel.Item>
                     <div style={ { backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.4)), url(/vendors/images/ws1.jpg)', height:
     '100vh', backgroundSize: 'cover', backgroundPosition: 'center'} }>
-                        {/* <img
-                        className="d-block w-100"
-                        src="/vendors/images/ws1.jpg"
-                        alt="First slide"
-                        /> */}
-                        <Carousel.Caption>
-                        <h1>First slide label</h1>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                        </Carousel.Caption>
+                        <div className="fadeInVertical">
+                            <Carousel.Caption>
+                                <h1>First slide label</h1>
+                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            </Carousel.Caption>
+                        </div>
                     </div>
                 </Carousel.Item>
                 <Carousel.Item>
                     <div style={ { backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.4)), url(/vendors/images/ws2.jpg)', height:
     '100vh', backgroundSize: 'cover', backgroundPosition: 'center' } }>
-                        {/* <img
-                        className="d-block w-100"
-                        src="/vendors/images/ws2.jpg"
-                        alt="Second slide"
-                        /> */}
-
-                        <Carousel.Caption>
-                        <h1>Second slide label</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </Carousel.Caption>
+                        <div className="fadeInVertical">
+                            <Carousel.Caption>
+                            <h1>Second slide label</h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </Carousel.Caption>
+                        </div>
                     </div>
                 </Carousel.Item>
                 <Carousel.Item>
                     <div style={ { backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.4)), url(/vendors/images/ws3.jpg)", height:
     "100vh", backgroundSize: 'cover', backgroundPosition: 'center' } }>
-                    {/* <img
-                    className="d-block w-100"
-                    src="/vendors/images/ws3.jpg"
-                    alt="Third slide"
-                    /> */}
-
-                    <Carousel.Caption>
-                    <h1>Third slide label</h1>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
-                    <div></div></div>
+                        <div className="fadeInVertical">
+                            <Carousel.Caption>
+                                <h1>Third slide label</h1>
+                                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                            </Carousel.Caption>
+                        </div>
+                    </div>
                 </Carousel.Item>
             </Carousel>
+        </div>
     );
 };
 
