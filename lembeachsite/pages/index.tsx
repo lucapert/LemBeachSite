5fda22326b00625c62f1d1cc3c7b284f41fe01cb
lucapert <l.perticaroli@gmail.com>
2021-03-30 22:59:26 +0200

    added news&event row

--- lembeachsite/pages/index.tsx
+++ lembeachsite/pages/index.tsx
@@ -2,13 +2,16 @@ import Styles from '../styles/Home.module.css';
 import CarouselReactB  from '../components/Carousel/Carousel';
 import Row from '../components/Row/Row';
 import Cards from '../components/Cards/Cards';
-import TextRow from '../components/TextRow/TextRow'
+import TextRow from '../components/TextRow/TextRow';
+import News from '../components/News/News';
 export default function Home() {
   return (
     <>
       <CarouselReactB />
       <Row backGroudColor="#ededed" children={ <TextRow title="MARE, SPORT, DIVERTIMENTO E TANTO ALTRO..." article="Al LEM troverai strutture sportive per poter tenerti sempre in allenamento. Corsi per tutti i livelli e tutte le fasce di età."  /> }/>
       <Row backGroudColor="#ffffff" children={ <Cards /> } />
+      <Row backGroudColor="#ededed" children={ <TextRow title="News, stay tuned" article="Gli ultimi eventi in programma..."  /> }/>
+      <Row backGroudColor="#ffffff" children={ <News /> } />
     </>
   )
 }
